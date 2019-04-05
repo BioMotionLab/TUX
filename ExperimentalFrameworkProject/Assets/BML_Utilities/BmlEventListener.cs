7d89df4b0db64b0e04eddf519991805860910b9e
AdamBebko <adambebko@gmail.com>
2019-04-05 16:34:08 -0400

    added black screen flashing

---
+++ ExperimentalFrameworkProject/Assets/BML_Utilities/BmlEventListener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace BML_Utilities {
+    public class BmlEventListener : MonoBehaviour {
+        public BmlEvent   Event;
+        public UnityEvent Response;
+
+        void OnEnable() {
+            Event.RegisterListener(this);
+        }
+
+        void OnDisable() {
+            Event.UnRegisterListener(this); ;
+        }
+
+        public void OnEventRaised() {
+            Response.Invoke();
+        }
+    }
+}
---
+++ ExperimentalFrameworkProject/Assets/BML_Utilities/FloatValue.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace BML_Utilities {
+
+    [CreateAssetMenu]
+    public class FloatValue : ScriptableObject {
+        public float Value;
+
+        public static implicit operator float(FloatValue value) {
+            return value.Value;
+        }
+
+    }
+
+}
