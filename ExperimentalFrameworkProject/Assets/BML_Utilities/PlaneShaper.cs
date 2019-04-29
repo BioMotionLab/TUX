129b87850692b62f0bd5748b5c5f6f777d6ae490
AdamBebko <adambebko@gmail.com>
2019-04-29 15:26:21 -0400

    merching submodules

---
+++ ExperimentalFrameworkProject/Assets/BML_Utilities/PlaneShaper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace BML_Utilities {
+    
+    [ExecuteInEditMode]
+    public class PlaneShaper : MonoBehaviour {
+
+        public float Width;
+        public float Height;
+        public bool LockAspect;
+
+        [Range(0, 1)]
+        public float Aspect;
+
+        void Update() {
+            transform.localScale = new Vector3(Width, Height, 1);
+
+            if (LockAspect) {
+                Height = Width * Aspect;
+            }
+
+         
+        }
+
+
+        /// <summary>
+        /// The Diagonal size of the AlbertiCanvasMount
+        /// </summary>
+        public float DiagonalSize {
+            get {
+                Vector2 diagonalVector = new Vector2(Width, Height);
+                float diagonalDistance = diagonalVector.magnitude;
+                return Mathf.Abs(diagonalDistance);
+            }
+        }
+
+        
+    }
+
+}
---
+++ ExperimentalFrameworkProject/Assets/BML_Utilities/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BML_Utilities {
+    public class Rotator : MonoBehaviour {
+
+        public float speed;
+        public int   frameCount = 0;
+
+    
+        // Update is called once per frame
+        void Update() {
+            frameCount++;
+            Debug.Log("FEFofjnasdlfnsad;l");
+        }
+    }
+}
