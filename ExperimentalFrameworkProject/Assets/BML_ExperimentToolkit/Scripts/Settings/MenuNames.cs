257cdd631f6b1e12d9f97aa1cf466597a53d0478
AdamBebko <adambebko@gmail.com>
2019-10-07 10:43:34 -0400

    changed menu names location

---
+++ ExperimentalFrameworkProject/Assets/BML_ExperimentToolkit/Scripts/Settings/MenuNames.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace BML_Utilities {
+    public static class MenuNames {
+        public const string BmlAssetMenu = "BML Assets/";
+        public const string BmlSettingsMenu = BmlAssetMenu + "Settings/";
+        public const string BmlMainMenu = "BML/";
+    }
+}
