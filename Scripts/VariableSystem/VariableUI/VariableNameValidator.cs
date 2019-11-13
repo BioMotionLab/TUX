9ab7bb7389068a9efe312f0f80fda5a1f146e379
AdamBebko <adambebko@gmail.com>
2019-11-13 16:54:21 -0500

    preparing to re-allow builds

---
+++ Scripts/VariableSystem/VariableUI/VariableNameValidator.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using bmlTUX.Scripts.VariableSystem.VariableValueAddingStrategies;
+
+namespace BML_TUX.Scripts.VariableSystem.VariableUI {
+    internal class VariableNameValidator {
+
+        public bool Valid => reasonsInvalid.Count == 0;
+
+        public string InvalidReasons {
+            get {
+                string reasons = "";
+                foreach (string reason in reasonsInvalid) {
+                    reasons += reason + "\n";
+                }
+
+                return reasons;
+            }
+        }
+
+        readonly List<string> reasonsInvalid = new List<string>();
+
+        const string InvalidName =
+            "Name Contains Illegal Characters. Name must be one word of letters or numbers only";
+
+        const string Unnamed = "Unamed variable.";
+        
+        public VariableNameValidator(string nameStringValue) {
+            if (!nameStringValue.All(c => Char.IsLetterOrDigit(c) || c == '_')) {
+                reasonsInvalid.Add(InvalidName);
+            }
+
+            if (nameStringValue == UnnamedColumn.Name) {
+                reasonsInvalid.Add(Unnamed);
+            }
+        }
+    }
+}
