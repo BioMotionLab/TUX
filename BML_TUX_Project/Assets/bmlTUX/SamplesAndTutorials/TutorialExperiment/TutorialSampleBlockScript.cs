2223e68d8e870f68ce759e94e5f2be829cde31d0
AdamBebko <adambebko@gmail.com>
2019-11-21 10:16:26 -0500

    fixed bug regarding helper tool, deleted documentation folder since out of date

---
+++ BML_TUX_Project/Assets/bmlTUX/SamplesAndTutorials/TutorialExperiment/TutorialSampleBlockScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using bmlTUX.Scripts.ExperimentParts;
+using UnityEngine;
+
+namespace bmlTUX.SamplesAndTutorials.TutorialExperiment {
+    public class TutorialSampleBlockScript : Block
+    {
+    
+        public TutorialSampleBlockScript(ExperimentRunner runner, DataTable trialTable, DataRow dataRow) : base(runner, trialTable, dataRow) {
+        }
+
+        protected override void PreMethod() {
+            //Get reference to custom ExperimentRunner
+            TutorialSampleExperimentRunner tutorialRunner = (TutorialSampleExperimentRunner) Runner;
+
+            //Get value of distance for this block.
+            float distance = (float) Data["Distance"];
+
+            //get position
+            Vector3 position = tutorialRunner.Stimulus.transform.localPosition;
+
+            //set z of position to the distance value.
+            position.z = distance;
+
+            //update stimulus position to new position
+            tutorialRunner.Stimulus.transform.localPosition = position;
+
+        }
+
+   
+    }
+}
