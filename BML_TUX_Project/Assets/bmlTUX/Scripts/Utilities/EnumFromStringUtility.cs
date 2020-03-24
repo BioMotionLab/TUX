b305569c9258b7f252ca5b4a11dc05c4bdbbda51
Adam Bebko <adambebko@gmail.com>
2020-03-24 17:31:19 -0400

    Addded EnumDataParseUtitlity

---
+++ BML_TUX_Project/Assets/bmlTUX/Scripts/Utilities/EnumFromStringUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace bmlTUX.Scripts.Utilities {
+    
+    [PublicAPI]
+    public static class EnumFromStringUtility {
+        
+        static T ExtractEnumFromData<T>(DataRow data, string enumVariableName) where T : struct, Enum  {
+            string value = (string) data[enumVariableName];
+            if (!Enum.TryParse(value, out T enumValue)) {
+                Debug.LogError($"Could not convert value {value} of variable {enumVariableName} to Enum. Check Spelling matches enum options.");
+            }
+            return enumValue;
+        }
+        
+    }
+}
