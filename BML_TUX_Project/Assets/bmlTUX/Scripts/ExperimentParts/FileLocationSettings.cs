ae1f286f5e32d20d24d64adbc365f6b9379d543a
Adam Bebko <adambebko@gmail.com>
2020-04-21 12:32:20 -0400

    added error detection for empty trial sequence, fixed file finding when packaged

--- BML_TUX_Project/Assets/bmlTUX/Scripts/ExperimentParts/FileLocationSettings.cs
+++ BML_TUX_Project/Assets/bmlTUX/Scripts/ExperimentParts/FileLocationSettings.cs
@@ -7,12 +7,42 @@ namespace bmlTUX.Scripts.ExperimentParts {
 	[CreateAssetMenu(menuName = "bmlTUX/File Location Settings")]
 	public class FileLocationSettings : ScriptableObject {
 
-		const string TuxProjectFolderPath = "/bmlTux/ScriptingTemplates/";
+		const string TuxFolderName = "bmlTux";
+		const string ScriptingTemplatesFolderName =	"ScriptingTemplates";
+		const string PackagePath = "Packages/com.biomotionlab.tux";
+		const string ExperimentRunnerTemplateFileName = "ExperimentRunnerTemplate.cs";
+		const string TrialTemplateFileName = "TrialScriptTemplate.cs";
+		const string BlockTemplateFileName = "BlockScriptTemplate.cs";
+		const string ExperimentTemplateFileName = "ExperimentScriptTemplate.cs";
+
+		public static string ExperimentRunnerTemplatePath => Path.Combine(TemplatePath , ExperimentRunnerTemplateFileName);
+		public static string TrialTemplatePath => Path.Combine(TemplatePath , TrialTemplateFileName);
+		public static string BlockTemplatePath => Path.Combine(TemplatePath , BlockTemplateFileName);
+		public static string ExperimentTemplatePath => Path.Combine(TemplatePath , ExperimentTemplateFileName);
 		
-		public static string TemplatePath => Application.dataPath + TuxProjectFolderPath;
+		static string TemplatePath => Path.Combine(TuxFolderPath, ScriptingTemplatesFolderName);
+
+		public static string TuxFolderPath {
+			get {
+				string tuxPath;
+				string tuxPackagePath = "";
+				string tuxAssetPath = Path.Combine(Application.dataPath, TuxFolderName);
+				tuxPath = tuxAssetPath;
+				if (!Directory.Exists(tuxPath)) {
+					tuxPackagePath = Path.GetFullPath(PackagePath);
+					tuxPath = tuxPackagePath;
+				}
+				
+				if (!Directory.Exists(tuxPath)) {
+					throw new DirectoryNotFoundException($"Can't find bmlTUX directory. Tried: {tuxAssetPath}, and {tuxPackagePath}");
+				}
+
+				return tuxPath;
+			}
+		}
+
 		
-		
-		
+
 		[SerializeField]
 		public string LastSessionSaveFileName = default;
 		
