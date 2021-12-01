d5d43ef81386b122ee817215df1f19406d523bd3
Adam Bebko <adambebko@gmail.com>
2021-11-30 23:18:02 -0500

    fix: adjusting UI namespaces

--- Scripts/UI/RuntimeUI/RunnerWindowUI/DesignFilePanel.cs
+++ Scripts/UI/RuntimeUI/RunnerWindowUI/DesignFilePanel.cs
@@ -1,7 +1,7 @@
 ﻿using TMPro;
 using UnityEngine;
 
-namespace bmlTUX.Scripts.UI.RuntimeUI.RunnerWindowUI {
+namespace bmlTUX.UI.RuntimeUI {
     public class DesignFilePanel : MonoBehaviour
     {
         [SerializeField]
