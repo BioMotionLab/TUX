023855655a7bdf8c3301cbea8b12290bf300715a
DerMilchmann <jakob.rhb@googlemail.com>
2022-02-10 14:38:50 +0100

    Fixed Custom Design File Location and Validation
    - UI Changed: Design File Path is now Design File Name
    - Added Design File Validation Result
    	- Checks if the the file exists
    	- Checks for .csv extension
    - Added InputFile
    - Modified Design File Panel to use OutputFolder as its base folder instead of Application root

---
+++ Scripts/Managers/InputFile.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+
+namespace bmlTUX{
+    public class InputFile{
+        public string InputFolder;
+        public string InputFilename;
+        public string FullPath;
+
+        public InputFile(string inputFolder, string inputFilename){
+            InputFolder = inputFolder;
+            InputFilename = inputFilename;
+            FullPath = Path.Combine(inputFolder, inputFilename);
+        }
+    }
+}
