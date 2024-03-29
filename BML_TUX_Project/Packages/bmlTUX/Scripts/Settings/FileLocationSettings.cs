c5ea5ab6aa8e7fbe4be01de182e5a375055b5c24
RhodeIfado <rhode@ifado.de>
2024-03-29 15:37:12 +0100

    Removed absolutePath bool

--- BML_TUX_Project/Packages/bmlTUX/Scripts/Settings/FileLocationSettings.cs
+++ BML_TUX_Project/Packages/bmlTUX/Scripts/Settings/FileLocationSettings.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 namespace bmlTUX {
   [Serializable]
 	public class FileLocationSettings  {
-        [Tooltip("Not every device supports absolute paths. Only advised for Builds on Windows and Mac.\nIf unchecked, Document Folder is used as root.")]
-        public bool dataFolderAsAbsolutePath = false;
         public string dataFolderName = "bmlTUX_Data";                
         public string SessionSaveFileName = "lastSession.json";  
         public string SessionLogFileName = "sessionLog.txt";
 
-        public string BaseTuxDocumentsFolderPath => dataFolderAsAbsolutePath ?
-            dataFolderName :
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), dataFolderName);
+        public string BaseTuxDocumentsFolderPath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), dataFolderName);
 
 		public string SessionFolder => BaseTuxDocumentsFolderPath;
 		public string DebugFolder => BaseTuxDocumentsFolderPath;
